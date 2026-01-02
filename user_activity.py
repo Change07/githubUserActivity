0ffd00f85dc38665f04da8d8a9657ab9ddc8b18c
kenneth <kenneth04@gmail.com>
2026-01-02 03:54:27 +0200

    simple REST API get request

---
+++ user_activity.py
@@ -0,0 +1,20 @@
+import requests
+import pandas as pd
+
+def main(username):
+    # get user activity
+    api_uri = f"https://api.github.com/users/{username}/events/public"
+
+    headers = {"username" : f"{username}"}
+
+    response = requests.get(api_uri, headers=headers).json()
+
+    print(response)
+
+    dataFrame = pd.DataFrame(response)
+    # print(dataFrame)
+    # print(dataFrame.values[0][3]["name"])
+    # display user activity 
+
+if __name__ == "__main__":
+    main("Change07")
